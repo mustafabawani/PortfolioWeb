aec19d2b609f3848bc7a4a7f1b9d3c5225f7a9f2
mustafa <mustafa_bawani@hotmail.com>
2023-04-11 02:12:02 +0500

    adding certification page

--- src/pages/certification/Certificationpg.js
+++ src/pages/certification/Certificationpg.js
@@ -11,7 +11,41 @@ function Certification(props) {
   const theme = props.theme;
   console.log(props.setTheme);
   return (
-    <></>
+    <div className="experience-main">
+      <Header theme={theme} setTheme={props.setTheme} />
+      <div className="basic-experience">
+        <Fade bottom duration={2000} distance="40px">
+          <div className="experience-heading-div">
+            <div className="experience-heading-img-div">
+              <ExperienceImg theme={theme} />
+            </div>
+            <div className="experience-heading-text-div">
+              <h1
+                className="experience-heading-text"
+                style={{ color: theme.text }}
+              >
+                {experience.title}
+              </h1>
+              <h3
+                className="experience-heading-sub-text"
+                style={{ color: theme.text }}
+              >
+                {experience["subtitle"]}
+              </h3>
+              <p
+                className="experience-header-detail-text subTitle"
+                style={{ color: theme.secondaryText }}
+              >
+                {experience["description"]}
+              </p>
+            </div>
+          </div>
+        </Fade>
+      </div>
+      <Certifications theme={props.theme} />
+<></>
+      <Footer theme={props.theme} onToggle={props.onToggle} />
+    </div>
   );
 }
 
