18d900077ebd5737eb5ba0bc68fba03b9624fdfd
mustafa <mustafa_bawani@hotmail.com>
2023-04-11 02:10:44 +0500

    adding home, project,education pages

---
+++ src/pages/certification/Certificationpg.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import Certifications from "../../containers/certifications/Certifications";
+import "./Certification.css";
+import { experience } from "../../portfolio.js";
+import { Fade } from "react-reveal";
+import ExperienceImg from "./CertificationImg";
+
+function Certification(props) {
+  const theme = props.theme;
+  console.log(props.setTheme);
+  return (
+    <></>
+  );
+}
+
+export default Certification;
---
+++ src/portfolio.js
@@ -0,0 +1,618 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: true, // Change this to true if you want to use the splash screen.
+  useCustomCursor: false, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hello.",
+  title2: "Mustafa",
+  logo_name: "mustafa.p()",
+  nickname: "mustafa2000 / bawani",
+  full_name: "MUHAMMAD MUSTAFA BAWANI",
+  subTitle:
+    "Backend Developer. Always learning.",
+  resumeLink:
+    "https://drive.google.com/file/d/1zXMidzjtt9U5a-uQ4W6eNX9BwumPOr_F/view?usp=sharing",
+  mail: "mailto:mustafa_bawani@hotmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/mustafabawani/",
+  linkedin: "https://www.linkedin.com/in/mustafabawani/",
+  gmail: "mustafa_bawani@hotmail.com",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Building responsive website front end using ReactJS",
+        "⚡ Developing mobile applications using Flutter and native Android",
+        "⚡ Creating application backend in Node, Express & Flask",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "PHP",
+          fontAwesomeClassname: "simple-icons:php",
+          style: {
+            color: "#7377AD",
+          },
+        },
+        {
+          skillName: "Java",
+          fontAwesomeClassname: "simple-icons:java",
+          style: {
+            color: "#f89820",
+          },
+        },
+        {
+          skillName: "Kotlin",
+          fontAwesomeClassname: "simple-icons:kotlin",
+          style: {
+            color: "#5c79df",
+          },
+        },
+        {
+          skillName: "Dart",
+          fontAwesomeClassname: "simple-icons:dart",
+          style: {
+            color: "#29B0EE",
+          },
+        },
+        {
+          skillName: "Go",
+          fontAwesomeClassname: "simple-icons:go",
+          style: {
+            color: "#00A7D0",
+          },
+        },
+        {
+          skillName: "Visual Basic",
+          fontAwesomeClassname: "simple-icons:dot-net",
+          style: {
+            color: "#029FCE",
+          },
+        },
+        {
+          skillName: "C#",
+          fontAwesomeClassname: "simple-icons:csharp",
+          style: {
+            backgroundColor: "transparent",
+            color: "#3776AB",
+          },
+        },
+        {
+          skillName: "C",
+          fontAwesomeClassname: "simple-icons:c",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C++",
+          fontAwesomeClassname: "simple-icons:cplusplus",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NextJS",
+          fontAwesomeClassname: "simple-icons:nextdotjs",
+          style: {
+            color: "#FFFFFF",
+            backgroundColor: "#1D1D1D",
+            borderRadius: "50%",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#439743",
+          },
+        },
+        {
+          skillName: "GraphQL",
+          fontAwesomeClassname: "simple-icons:graphql",
+          style: {
+            color: "#DE33A6",
+          },
+        },
+        {
+          skillName: "Android App Development",
+          fontAwesomeClassname: "simple-icons:android",
+          style: {
+            color: "#3DDC84",
+          },
+        },
+        {
+          skillName: "Flutter",
+          fontAwesomeClassname: "simple-icons:flutter",
+          style: {
+            color: "#02569B",
+          },
+        },
+        {
+          skillName: "MySQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#4479A1",
+          },
+        },
+
+        {
+          skillName: "Wordpress",
+          fontAwesomeClassname: "simple-icons:wordpress",
+          style: {
+            color: "#207297",
+          },
+        },
+
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+      ],
+    },
+    {
+      title: "Data Science & AI",
+      fileName: "DataScienceImg",
+      skills: [
+        "⚡ Experience of working with Computer Vision and NLP projects",
+        "⚡ Certifications by deeplearning.ai and Stanford Online",
+        "⚡ Experience with 10+ Projects",
+      ],
+      softwareSkills: [
+        {
+          skillName: "Tensorflow",
+          fontAwesomeClassname: "logos-tensorflow",
+          style: {
+            backgroundColor: "transparent",
+          },
+        },
+        {
+          skillName: "Keras",
+          fontAwesomeClassname: "simple-icons:keras",
+          style: {
+            backgroundColor: "white",
+            color: "#D00000",
+          },
+        },
+        {
+          skillName: "PyTorch",
+          fontAwesomeClassname: "logos-pytorch",
+          style: {
+            backgroundColor: "transparent",
+          },
+        },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "ion-logo-python",
+          style: {
+            backgroundColor: "transparent",
+            color: "#3776AB",
+          },
+        },
+        {
+          skillName: "Pandas",
+          fontAwesomeClassname: "simple-icons:pandas",
+          style: {
+            backgroundColor: "transparent",
+            color: "#6E6E6E",
+          },
+        },
+      ],
+    },
+    {
+      title: "Cloud Infra-Architecture",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experience working on multiple cloud platforms",
+        "⚡ Experience hosting and managing websites",
+        "⚡ Deploying deep learning models on cloud to use on mobile devices",
+        "⚡ Experience with Continuous Integration",
+      ],
+      softwareSkills: [
+        {
+          skillName: "GCP",
+          fontAwesomeClassname: "simple-icons:googlecloud",
+          style: {
+            color: "#4285F4",
+          },
+        },
+        {
+          skillName: "AWS",
+          fontAwesomeClassname: "simple-icons:amazonaws",
+          style: {
+            color: "#FF9900",
+          },
+        },
+        {
+          skillName: "Netlify",
+          fontAwesomeClassname: "simple-icons:netlify",
+          style: {
+            color: "#38AFBB",
+          },
+        },
+        {
+          skillName: "Heroku",
+          fontAwesomeClassname: "simple-icons:heroku",
+          style: {
+            color: "#6863A6",
+          },
+        },
+        {
+          skillName: "Firebase",
+          fontAwesomeClassname: "simple-icons:firebase",
+          style: {
+            color: "#FFCA28",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+          },
+        },
+        {
+          skillName: "Docker",
+          fontAwesomeClassname: "simple-icons:docker",
+          style: {
+            color: "#1488C6",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Fast Nuces University",
+      subtitle: "Bachelors in Computer Science",
+      alt_name: "Fast",
+      duration: "2019 - 2023",
+      },
+    {
+      title: "Army Public College",
+      subtitle: "FSC in pr-engineering",
+      alt_name: "APS",
+      duration: "2017 - 2019",
+      },
+      {
+        title: "Meritorious school network",
+        subtitle: "O-levels",
+        alt_name: "APS",
+        duration: "2015 - 2017",
+        },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "Web designing",
+      subtitle: " ",
+      logo_path: "NoorCollege.png",
+      certificate_link: " ",
+      alt_name: "Google",
+      color_code: "#ffc475",
+    },
+    {
+      title: "PHP developer",
+      subtitle: " ",
+      logo_path: "Aptech_Limited_Logo.svg.png",
+      certificate_link: " ",
+      alt_name: "Google",
+      color_code: "#ffc475",
+    },
+    {
+      title: "Scrum Foundation Professional ",
+      subtitle: "",
+      logo_path: "CertiProf_Logo_250x.avif",
+      certificate_link: " ",
+      alt_name: "Google",
+      color_code: "#ffc475",
+    },
+    {
+      title: "Database Competition head",
+      subtitle: "",
+      logo_path: "devday.png",
+      certificate_link: " ",
+      alt_name: "Google",
+      color_code: "#ffc475",
+    },
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Certifications",
+  subtitle: " ",
+  description:
+    "A certification is a testament to the hard work and dedication put into acquiring specialized knowledge and skills in a particular field. By earning a certification, I have demonstrated my commitment to professional development",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work",
+      experiences: [
+        {
+          title: "Full Stack Developer",
+          company: "Quicko",
+          company_url: "https://quicko.com/",
+          logo_path: "quicko.jpg",
+          duration: "June 2021 - October 2021",
+          location: "Ahmedabad",
+          description:
+            "Quicko is an online tax planning, preparation & filing platform for individuals & businesses.",
+          color: "#2962FF",
+        },
+      ],
+    },
+    {
+      title: "Internships",
+      experiences: [
+        {
+          title: "Google Summer of Code",
+          company: "Sugar Labs",
+          company_url: "https://sugarlabs.org/",
+          logo_path: "google_logo.png",
+          duration: "May 2019 - Aug 2019",
+          location: "Work From Home",
+          description:
+            "I worked on the Dashboard project which helps users track their activities while using Sugar OS. I also worked on making a Tamagotchi-like widget for Sugar's Home Screen",
+          color: "#ee3c26",
+        },
+        {
+          title: "Android App Developer Intern",
+          company: "IAS4Sure",
+          company_url: " ",
+          logo_path: "buld.jpg",
+          duration: "December 2019 - February 2020",
+          location: "Work From Home",
+          description:
+            "Internship task was to make a native Android application. ",
+          color: "#0071C5",
+        },
+        {
+          title: "Web Developer",
+          company: "Wrighter Writing Solutions",
+          company_url: " ",
+          logo_path: "wrighter.jpg",
+          duration: "August 2020",
+          location: "",
+          description: "Develop a website using PHP and jQuery.",
+          color: "#56A4D3",
+        },
+        {
+          title: "Front-End Developer",
+          company: "VJ TechServe",
+          company_url: " ",
+          logo_path: "vjt.png",
+          duration: "September 2020",
+          location: "",
+          description: "Develop a portfolio website using ReactJS",
+          color: "#800000",
+        },
+      ],
+    },
+    {
+      title: "Volunteerships",
+      experiences: [
+        {
+          title: "Google Summer of Code Mentor",
+          company: "Sugar Labs / GSoC",
+          company_url: "https://sugarlabs.org/",
+          logo_path: "google_logo.png",
+          duration: "Summer of 2017 / 18 / 20",
+          location: " ",
+          description:
+            "Mentorship responsibilities were to help students plan the project, review issues and pull requests, ensure smooth progress and help them out when they are stuck.",
+          color: "#4285F4",
+        },
+        {
+          title: "Google Code-In Mentor",
+          company: "Sugar Labs / GSoC",
+          company_url: "https://sugarlabs.org/",
+          logo_path: "google_logo.png",
+          duration: "Winter of 2017 / 18 / 19",
+          location: " ",
+          description:
+            "Day to day resposibilites of this mentorship was to help out children aged 13-17 get started with programming and open-source, review their work and approve them.",
+          color: "#D83B01",
+        },
+        {
+          title: "Board Member at Codeuino",
+          company: " ",
+          company_url: " ",
+          logo_path: "codeuino.jpg",
+          duration: "2018 - 2019",
+          location: " ",
+          description: "Previous Board during early days of Codeuino",
+          color: "#D83B01",
+        },
+        {
+          title: "BluWings Club",
+          company: " ",
+          company_url: " ",
+          logo_path: "b.png",
+          duration: "2018 - On Going",
+          location: " ",
+          description:
+            "Co-Founder and President of the programming club called BluWings. Have organized several workshops which introduced programming and related tools to freshers.",
+          color: "#D83B01",
+        },
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects makes use of vast variety of latest technology tools. Below are some of my projects. Note that not all of the mentioned projects are on GitHub yet.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "mustafa2.jpeg",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  // blogSection: {
+  //   title: "Blogs",
+  //   subtitle:
+  //     "I don't blog frequently but when I do something awesome, I do try to document it so it can be helpful to others. I write on Medium.",
+  //   link: "https://medium.com/",
+  //   avatar_image_path: "blogs_image.svg",
+  // },
+};
+
+const projects = {
+  data: [
+    {
+      name: "Parsify (FYP)",
+      url: "#",
+      description:
+        "Desktop based tool for Data Extraction and data Analysis that allows the user to extract data from any given website and get meaningful insights on the data extracted",
+      languages: [
+        {
+          name: "Reactjs",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Flask",
+          iconifyClass: "logos-flask",
+        },
+        {
+          name: "Electron",
+          iconifyClass: "logos-electron",
+        },
+      ],
+    },
+    {
+      name: "Teezigner",
+      url: "#",
+      description: "A web based tee-shirt store supporting buyer and seller interface. It helped seller to design graphic tee-shirt and earn profits on each sell for his design",
+      languages: [
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "ExpressJS",
+          iconifyClass: "logos-express",
+        },
+        {
+          name: "NodeJS",
+          iconifyClass: "logos-nodejs",
+        },
+        {
+          name: "MySQL",
+          iconifyClass: "logos-mysql",
+        },
+      ],
+    },
+    {
+      name: " Implementing Least Distance Algorithm for Search Engine",
+      url: "#",
+      description: "A simple search engine where user queries a word and retrieves meaning of that word from the dictionary.If the query word doesn’t match any word inside the dictionary the algorithm suggests the user with related words",
+      languages: [
+        {
+          name: "C++",
+          iconifyClass: "simple-icons:cplusplus",
+        },
+      ],
+    },
+    {
+      name: "Hospital Management System",
+      url: " ",
+      description:
+        "Console based Management system made in C++ using some advance concepts of OOP such as Inheritance, Abstraction, Operator Overloading",
+      languages: [
+        {
+          name: "C++",
+          iconifyClass: "simple-icons:cplusplus",
+        },
+      ],
+    },
+    {
+      name: "Arthimetic Syntax Checker",
+      url: " ",
+      description:
+        "Implemented LL(1) parser to check if the input arithmetic string is valid or not in C Language",
+      languages: [
+        {
+          name: "C",
+          iconifyClass: "logos-c",
+        },
+      ],
+    },
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
